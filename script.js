f188678a9239d74c3044e3e8265e413a85386590
spal45 <141572211+spal45@users.noreply.github.com>
2024-05-22 08:10:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('previewButton').addEventListener('click', function() {
+    window.open('image/Subhankar PAL(RESUME).pdf', '_blank');
+  });
+
+
+  var navLinks = document.getElementById("navLinks");
+      function showMenu() {
+        navLinks.style.right = "0";
+      }
+      function hideMenu() {
+        navLinks.style.right = "-200px";
+      }
